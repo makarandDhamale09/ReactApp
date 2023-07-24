33957877fbac6369e7aa95d0abefe4a8f6ead729
makaranddhamale <makarand.dhamale09@gmail.com>
2023-07-24 19:23:44 +0530

    Search Implemented

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,8 @@
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "7b3f9e5027msh9e0e6555e8abb72p1e74b2jsnf50ebdd4eeec",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
