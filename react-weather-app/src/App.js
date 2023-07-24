535a47337bd6ccd80e6f9becaec668b9e92926f2
makaranddhamale <makarand.dhamale09@gmail.com>
2023-07-24 21:32:42 +0530

    Current weather implemention completed

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -1,15 +1,42 @@
 import "./App.css";
+import { useState } from "react";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 import CurrentWeather from "./components/current-weather/current-weather";
 import Search from "./components/search/search";
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecastWeather, setForecastWeather] = useState(null);
+
   const handleOnSearchChange = (searchData) => {
-    const [lat, long] = searchData.value.split(" ");
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecastWeather({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => console.log(err));
   };
+
+  console.log(currentWeather);
+  console.log(forecastWeather);
+
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
-      <CurrentWeather />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
     </div>
   );
 }
--- react-weather-app/src/api.js
+++ react-weather-app/src/api.js
@@ -6,3 +6,7 @@ export const geoApiOptions = {
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "7fbd31e4df69825c0bbbbe9b89245a41";
