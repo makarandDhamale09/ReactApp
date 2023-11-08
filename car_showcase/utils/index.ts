40f3951eff36ccbbde431d8867bf97bcfd99e45d
makaranddhamale <makarand.dhamale09@gmail.com>
2023-11-08 22:18:29 +0530

    Added image url

--- car_showcase/utils/index.ts
+++ car_showcase/utils/index.ts
@@ -1,4 +1,5 @@
 import { CarProps } from "@/types";
+import { url } from "inspector";
 
 export async function fetchCars() {
   const headers = {
@@ -33,4 +34,18 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  //hrjavascript-mastery
+  const url = new URL("https://cdn.imagin.studio/getimage");
+
+  const { make, year, model } = car;
+
+  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
 };
