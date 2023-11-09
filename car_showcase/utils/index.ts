6148d666866135344393527d456bf497f4e11b3c
makaranddhamale <makarand.dhamale09@gmail.com>
2023-11-09 20:39:52 +0530

    Added search functionality

--- car_showcase/utils/index.ts
+++ car_showcase/utils/index.ts
@@ -1,14 +1,14 @@
-import { CarProps } from "@/types";
-import { url } from "inspector";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
   const headers = {
     "X-RapidAPI-Key": "2419409df5msh0f0c0335c6a58a7p1ea6f7jsn3253821fcdab",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
   const response = fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera",
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     {
       headers: headers,
     }
@@ -48,4 +48,6 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   url.searchParams.append("zoomType", "fullscreen");
   url.searchParams.append("modelYear", `${year}`);
   url.searchParams.append("angle", `${angle}`);
+
+  return `${url}`;
 };
